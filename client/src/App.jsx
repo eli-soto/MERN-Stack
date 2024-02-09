bba5bc630e6d9bb24bfeb7f7d00db93bfe224c2c
eli-soto <142184979+eli-soto@users.noreply.github.com>
2024-02-08 19:20:23 -0500

    debugging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
+import Home from './pages/Home'
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -11,6 +11,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Home />
         <Outlet />
       </div>
     </ApolloProvider>
