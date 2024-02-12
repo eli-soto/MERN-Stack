356c7d5b4b01951ba3ae621fe660adb9671260be
eli-soto <142184979+eli-soto@users.noreply.github.com>
2024-02-12 18:50:08 -0500

    navbar

---
+++ client/src/pages/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
