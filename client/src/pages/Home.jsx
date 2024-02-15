bdb26142414673ebf770ca7d992bdcb130af3580
eli-soto <142184979+eli-soto@users.noreply.github.com>
2024-02-14 21:58:43 -0500

    seeding database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,9 +4,9 @@ import ProductList from './ProductList';
 const Home = () => {
   return (
     <div>
-       <h1 className="text-4xl text-center font-bold">Welcome to our eCommerce Store!</h1>
-      <h1></h1>
-      {/* <ProductList /> */}
+       <h1 className="text-4xl text-center font-bold">Welcome to our SHOE eCommerce Store!</h1>
+    
+      <ProductList/>
     </div>
   );
 };
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -13,6 +13,7 @@ console.log(data);
     <div className="max-container padding-y">
       <h2 className="text-2xl font-bold text-center mb-8">All Products</h2>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {console.log(data)}
         {data.getAllProduct.map(product => (
           <div key={product._id} className="bg-white rounded-lg shadow-md overflow-hidden">
             <div className="p-6">
